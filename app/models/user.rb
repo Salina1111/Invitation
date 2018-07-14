cb701c784e2995dd61aba085c9856dba14e60e5e
Salina <salina.sallu@outlook.com>
2018-07-14 23:47:14 +0545

    instruction_added_And_admin_made

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   
-
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 2.weeks
-
+  :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 2.weeks
+  
   validates :firstname, presence: true       
   validates :lastname, presence: true
   validates :roll_number, presence: true
@@ -13,6 +13,20 @@ class User < ApplicationRecord
   validates :faculty, presence: true
   validates :phone_number, presence: true
   validates :address, presence: true
-
-
+  
+  
+  has_many :invitations, :class_name => 'User', :as => :invited_by
+  
+  
+  def email_admins
+    # ...
+  end
+  
+  def email_invited_by
+    # ...
+  end
+  
+  
+  
+  
 end
